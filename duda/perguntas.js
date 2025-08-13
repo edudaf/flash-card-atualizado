ff50302014b5c0540c32c7ef79f38e95d3f763cd
edudaf <eduarda.ferreira09@escola.pr.gov.br>
2025-08-13 09:00:13 -0300

    Add files via upload

---
+++ duda/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Perguntas Objetivas',
+    'O que é Administração?',
+    ' é a ciência e a arte de planejar, organizar, dirigir e controlar recursos (humanos, financeiros, materiais e informacionais) para alcançar objetivos de forma eficaz e eficiente'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Nova Iorque?',
+    'A capital de nova iorque é Albany.'
+)
+
+criaCartao(
+    'história',
+    'Quem pintou a Mona lisa?',
+    'Leonardo da Vinci'
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
